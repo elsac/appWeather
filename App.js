8e5ab6319aa9b58dba614399e087f131253b5f00
elsac <carto.elsa@gmail.com>
2019-03-26 11:00:49 +0100

    push app

---
+++ App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
+import { API_KEY } from './utils/WeatherAPIKey';
+
+import Weather from './components/Weather';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error Getting Weather Condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat, lon) {
+    fetch(
+      `https://openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metrics`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          town: json.name,
+          isLoading: false
+        });
+      });
+  }
+  
+  render() {
+    const { isLoading, weatherCondition, temperature, town } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? (
+          <View style={styles.loadingContainer}>
+            <Text style={styles.loadingText}>Fetching The Weather</Text>
+          </View>
+        ) : (
+          <Weather weather={weatherCondition} town={town} temperature={temperature} />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFDE4'
+  },
+  loadingText: {
+    fontSize: 30
+  }
+});
